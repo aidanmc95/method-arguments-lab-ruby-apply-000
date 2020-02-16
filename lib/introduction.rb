23ecea97b406e12e437112ce3605ffc10ad7d525
Aidan Muller-Cohn <aidanmc95@gmail.com>
2020-02-16 21:39:18 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,11 @@
+# Method Arguments Lab
+
+## This method takes in name:string and out puts a sentence
+def introduction(name)
+  puts "Hi, my name is #{name}.\n"
+end
+
+## This mothd takes in name:string and language:string and outputs a sentence
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
