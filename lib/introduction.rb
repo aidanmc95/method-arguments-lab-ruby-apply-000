6d478d7117b47bbb5ab962d018635140e5606e2c
Aidan Muller-Cohn <aidanmc95@gmail.com>
2020-02-20 15:07:27 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,10 +2,10 @@
 
 ## This method takes in name:string and out puts a sentence
 def introduction(name)
-  puts "Hi, my name is #{name}"
+  puts "Hi, my name is #{name}.\n"
 end
 
 ## This mothd takes in name:string and language:string and outputs a sentence
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
